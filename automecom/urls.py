476dccc9ba5e5d61c2c6491c42ca3a98b3c1aeb9
a22004247 <4124.g17.gfp@gmail.com>
2023-04-27 19:56:53 +0100

    servicoForm

--- automecom/urls.py
+++ automecom/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = "automecom"
 urlpatterns = [
-    path('', views.home_view, name="Home")]
+    path('home.html', views.home_view, name="Home"),
+    path('servico.html', views.servico_view, name="Servico"),
+    path('create.html', views.servico_create, name="Create"),
+    path('edit/<int:post_id>', views.servico_edit, name='editar'),
+    path('delete/<int:post_id>', views.servico_delete, name='apagar'),
+]
--- automecom/views.py
+++ automecom/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 from .forms import ServicoForm
 from .models import Servico
@@ -8,29 +10,36 @@ def home_view(request):
     return render(request, 'automecom/home.html')
 
 
-def servico_list(request):
+def servico_view(request):
     servicos = Servico.objects.all()
-    return render(request, 'automecom/home.html', {'servicos': servicos})
+    return render(request, 'automecom/servico.html', {'servicos': servicos})
 
 
 def servico_create(request):
     if request.method == 'POST':
-        form = ServicoForm(request.POST)
+        form = ServicoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('servico_list')
-    else:
-        form = ServicoForm()
-    return render(request, 'automecom/home.html', {'form': form})
+            return HttpResponseRedirect(reverse('automecom:Servico'))
+    form = ServicoForm()
+    context = {'form': form}
+    return render(request, 'automecom/create.html', context)
 
 
-def servico_edit(request, pk):
-    servico = get_object_or_404(Servico, pk=pk)
+def servico_edit(request, post_id):
+    post = Servico.objects.get(id=post_id)
     if request.method == 'POST':
-        form = ServicoForm(request.POST, instance=servico)
+        form = ServicoForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             form.save()
-            return redirect('servico_list')
+            return HttpResponseRedirect(reverse('automecom:Servico'))
     else:
-        form = ServicoForm(instance=servico)
-    return render(request)
+        form = ServicoForm(instance=post)
+    context = {'form': form, 'post_id': post_id}
+    return render(request, 'automecom/edit.html', context)
+
+
+def servico_delete(request, post_id):
+    if Servico.objects.filter(pk=post_id).exists():
+        Servico.objects.get(pk=post_id).delete()
+    return HttpResponseRedirect(reverse('automecom:Servico'))
